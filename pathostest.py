4b482e66899e67c68b286ce937ad2eb11a16ea5e
Jack Peters <jackjt8@gmail.com>
2017-11-15 22:00:56 +0000

    Testing pathos with itertools
    repeat(var) works as expected.

--- pathostest.py
+++ pathostest.py
@@ -1,26 +1,28 @@
 from pathos.multiprocessing import ProcessingPool as Pool
+from itertools import repeat
 
 p = Pool(4)
 class Test(object):
-    def plus(self, x, y): 
-        return x+y
+    def plus(self, x, y, z): 
+        return x,y,z
  
     
 x = [2,4,6,8]
 y = [1,3,5,7]
+z = [3,2,1,0]
 
 t = Test()
-p.map(t.plus, x, y)
-[4, 6, 8, 10]
+temp = p.map(t.plus, repeat(x), y, z)
+#[4, 6, 8, 10]
  
-class Foo(object):
-    @staticmethod
-    def work(self, x):
-        return x+1
-
-f = Foo()
-p.apipe(f.work, f, 100)
-#<processing.pool.ApplyResult object at 0x10504f8d0>
-res = _
-res.get()
-101
+#class Foo(object):
+#    @staticmethod
+#    def work(self, x):
+#        return x+1
+#
+#f = Foo()
+#p.apipe(f.work, f, 100)
+##<processing.pool.ApplyResult object at 0x10504f8d0>
+#res = _
+#res.get()
+#101
