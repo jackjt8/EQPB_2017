096ba238553483aeae9e4cad563781472ad3d081
Jack Peters <jackjt8@gmail.com>
2017-11-15 22:05:21 +0000

    fixed script so it works in console
    Needed to add freeze_support() to if __name__ == '__main__': to let it work on windows. May need to remove this for other OSes.

--- pathostest.py
+++ pathostest.py
@@ -1,28 +1,35 @@
 from pathos.multiprocessing import ProcessingPool as Pool
+from pathos.helpers import freeze_support
 from itertools import repeat
 
-p = Pool(4)
-class Test(object):
-    def plus(self, x, y, z): 
-        return x,y,z
- 
+def main():
+    p = Pool(4)
+    class Test(object):
+        def plus(self, x, y, z): 
+            return x,y,z
+     
+        
+    x = [2,4,6,8]
+    y = [1,3,5,7]
+    z = [3,2,1,0]
     
-x = [2,4,6,8]
-y = [1,3,5,7]
-z = [3,2,1,0]
-
-t = Test()
-temp = p.map(t.plus, repeat(x), y, z)
-#[4, 6, 8, 10]
- 
-#class Foo(object):
-#    @staticmethod
-#    def work(self, x):
-#        return x+1
-#
-#f = Foo()
-#p.apipe(f.work, f, 100)
-##<processing.pool.ApplyResult object at 0x10504f8d0>
-#res = _
-#res.get()
-#101
+    t = Test()
+    temp = p.map(t.plus, repeat(x), y, z)
+    print temp
+    #[4, 6, 8, 10]
+     
+    #class Foo(object):
+    #    @staticmethod
+    #    def work(self, x):
+    #        return x+1
+    #
+    #f = Foo()
+    #p.apipe(f.work, f, 100)
+    ##<processing.pool.ApplyResult object at 0x10504f8d0>
+    #res = _
+    #res.get()
+    #101
+    
+if __name__ == '__main__':
+    freeze_support()
+    main()
