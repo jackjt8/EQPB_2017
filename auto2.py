97e98439441629b5fb8af3d1f8d1ae7123133173
Jack Peters <jackjt8@gmail.com>
2017-11-17 12:55:05 +0000

    Floating point range fix
    Fixed floating point range issues by making an integer range and then dividing by 1000 to get the correct floating points.

--- auto2.py
+++ auto2.py
@@ -6,14 +6,15 @@ from pathos.helpers import freeze_support
 
 
 def main():
-    start_date = datetime(2016,1,1,0,0,0);
-    end_date = datetime(2017,1,1,0,0,0);
+    start_date = datetime(2015,10,20,0,0,0);
+    end_date = datetime(2017,1,10,0,0,0);
     localpath = 'D:\\jackj\\Documents\\GitHub\\EQPB_2017\\'
-    satlist = [41,48]
+    satlist = [73]
     
-    L_thres = np.arange(0.01,0.07,0.01)
+    #L_thres = np.arange(0.000,0.070,0.001) # 0.000 might return nothing, but we might have data there...
+    L_thres = np.arange(0.0,70.0,5.0) / 1000 # floating point fun fix. Work with Int then divide down.
     #L_thres = [0.005,0.03,0.07,0.10,0.13,0.17]
-    alt2test = [i * 100 for i in [1,4,8,12,16,20]]
+    alt2test = [i * 100 for i in range(20)]
     maxsizeondisk = 100 # given in GB.
     threads = 6
 
